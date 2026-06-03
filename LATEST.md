dce673e3aa115fbdc169702ee0ad1c956f0f6d98
doombubbles <obsidiantoolzg@gmail.com>
2026-06-02 20:23:06 -0700

    Recompile for BTD6 v55

--- LATEST.md
+++ LATEST.md
@@ -1,6 +1,1 @@
-## New functionality for Extended Vanilla Paths
-
-- Updated internals to use the new `ModMutator` class added with Mod Helper 3.6.0
-- Fixed a few edge cases with `ExtendVanillaPath` paths
-- Added a default `ModTest` for paths++
-- Fixed interaction between Dart Monkey paths++ and Geraldo's Super Monkey transform effect
+- Recompiled for BTD6 v55
--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,8 +2,8 @@ namespace PathsPlusPlus;
 
 internal static class ModHelperData
 {
-    public const string WorksOnVersion = "54.3";
-    public const string Version = "1.2.1";
+    public const string WorksOnVersion = "55.0";
+    public const string Version = "1.2.2";
     public const string Name = "Paths++";
 
     public const string Description = "A helper mod allowing additional upgrade paths and tiers to be made for towers.\n\n" +
